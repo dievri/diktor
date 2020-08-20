cbfcf8f2a96aa92573b1fb4fc9311e4e4c2c798e
Oleksandr Dievri <o.dievri@gmail.com>
2020-08-20 16:06:18 +0300

    replace awk with sed

--- add_diktor.sh
+++ add_diktor.sh
@@ -1,7 +1,3 @@
-sudo wget -P /usr/share/X11/xkb/symbols/ https://raw.githubusercontent.com/dievri/diktor/master/linux/diktor
-awk '/! layout/{print;print "diktor    Russian Typewriter Diktor";next}1' /usr/share/X11/xkb/rules/evdev.lst > tmp && mv tmp /usr/share/X11/xkb/rules/evdev.lst
-awk '/! layout/{print;print "diktor    Russian Typewriter Diktor";next}1' /usr/share/X11/xkb/rules/base.lst > tmp && mv tmp /usr/share/X11/xkb/rules/base.lst
-awk '/\<layoutList\>/{print;print "<layout><configItem><name>diktor</name><shortDescription>diktor</shortDescription><description>Typewriter Russian Diktor</description><languageList><iso639Id>rus</iso639Id></languageList></configItem><variantList/></layout>";next}1' /usr/share/X11/xkb/rules/base.xml > tmp && mv tmp /usr/share/X11/xkb/rules/base.xml 
-awk '/\<layoutList\>/{print;print "<layout><configItem><name>diktor</name><shortDescription>diktor</shortDescription><description>Typewriter Russian Diktor</description><languageList><iso639Id>rus</iso639Id></languageList></configItem><variantList/></layout>";next}1' /usr/share/X11/xkb/rules/evdev.xml > tmp && mv tmp /usr/share/X11/xkb/rules/evdev.xml
-
-
+wget -P /usr/share/X11/xkb/symbols/ https://raw.githubusercontent.com/dievri/diktor/master/linux/diktor
+sed -i'' '/! layout/a diktor    Russian Typewriter Diktor' /usr/share/X11/xkb/rules/evdev.lst /usr/share/X11/xkb/rules/base.lst
+sed -i'' '/<layoutList\>/a <layout><configItem><name>diktor</name><shortDescription>diktor</shortDescription><description>Typewriter Russian Diktor</description><languageList><iso639Id>rus</iso639Id></languageList></configItem><variantList/></layout>' /usr/share/X11/xkb/rules/evdev.xml /usr/share/X11/xkb/rules/base.xml
